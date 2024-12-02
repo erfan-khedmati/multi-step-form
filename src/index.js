60f4e470e82c557cd078f876a3eaeb3ff7eb02eb
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-12-03 00:05:34 +0330

    install react router and configiure home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// React Router
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
