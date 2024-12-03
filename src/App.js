a485c5f04e10bd08bc2f564ed2a96adbe8b039d7
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-12-03 20:58:20 +0330

    setup nested routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 // bg-alabaster
 function App() {
@@ -5,6 +7,7 @@ function App() {
     <div className="w-screen h-screen flex justify-center items-center bg-alabaster"> 
       <div className="max-w-[700px] bg-white p-5 rounded-2xl">
         <Navbar />
+        <Outlet />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import './index.css';
 import App from './App';
 
 // React Router
+import Page1 from './Pages/Page1';
+import Page2 from './Pages/Page2';
+import Page3 from './Pages/Page3';
+import Page4 from './Pages/Page4';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    children: [
+      { index: true, element: <Page1 />, },
+      { path: "/page2", element: <Page2 />, },
+      { path: "/page3", element: <Page3 />, },
+      { path: "/page4", element: <Page4 />, },
+
+    ]
   }
 ])
 
