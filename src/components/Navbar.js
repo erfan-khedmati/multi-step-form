461b21958387dcda338f72e54fa5ee2c281940c1
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-12-04 17:11:38 +0330

    update navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,18 +31,16 @@ export default function Navbar() {
         <div className='max-h-[550px] h-screen relative pl-10 pr-32 py-1 rounded-xl bg-slate-300 overflow-hidden'>
             <div className='z-20 relative w-full h-full'>
                 {steps.map((step) => {
-                    return(
-                    <div className='flex items-center gap-4 text-white py-5' key={step.id}>
-                        <NavLink to={step.id != 1 ? `/page${step.id}` : "/"} className={({isActive})=> isActive ? "flex items-center justify-center rounded-full w-8 h-8 text-marine_blue bg-light_blue" : "flex items-center justify-center border-2 border-light_blue rounded-full w-8 h-8"}>
-                            {step.id}
-                        </NavLink>
-                        <div className='flex flex-col'>
-                            <p className='text-light_gray text-xs'>{step.placeHolder}</p>
-                            <p className='font-bold text-sm'>{step.titel}</p>
-                        </div>
-                    </div>)
-
-
+                    return (
+                        <div className='flex items-center gap-4 text-white py-3' key={step.id}>
+                            <NavLink to={step.id != 1 ? `/page${step.id}` : "/"} className={({ isActive }) => isActive ? "flex items-center justify-center rounded-full w-8 h-8 font-bold text-marine_blue bg-light_blue" : "flex items-center justify-center font-bold border-2 border-light_blue rounded-full w-8 h-8"}>
+                                {step.id}
+                            </NavLink>
+                            <div className='flex flex-col'>
+                                <p className='text-light_gray text-xs'>{step.placeHolder}</p>
+                                <p className='font-bold text-sm'>{step.titel}</p>
+                            </div>
+                        </div>)
                 })}
             </div>
             <img src={SideBarImg} className='absolute bottom-0 left-0 w-full z-10' />
