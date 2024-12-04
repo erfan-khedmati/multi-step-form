7885c19cfdbd366f6baa877f944bc68388476365
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-12-04 17:31:42 +0330

    make App component responsive for mobile

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navbar from "./components/Navbar";
 // bg-alabaster
 function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-alabaster"> 
-      <div className="max-w-[700px] bg-white p-5 rounded-2xl">
+    <div className="w-screen h-screen flex justify-center items-center bg-alabaster">
+      <div className="w-full h-full lg:max-w-[1200px] lg:h-auto bg-white p-5 rounded-2xl flex">
         <Navbar />
         <Outlet />
       </div>
